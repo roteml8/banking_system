770a16b01291bf3e4c476f3dfb05380fafeedf82
roteml8 <missroteml@gmail.com>
2022-04-27 13:04:34 +0300

    generated java doc

---
+++ doc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"banking_system.app"},{"l":"banking_system.banking"},{"l":"banking_system.users"}];updateSearchResults();
